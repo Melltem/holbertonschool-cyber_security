411c64bf6e72ba5fc4169315e7ea18681adc16ac
Melltem <meltemqasimli@gmail.com>
2025-08-11 20:01:44 +0000

    0. What secrets hold

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "Last 5 completed login sessions:\n$"(last | grep -vE "reboot|still logged in" | head -n 5 | awk '{print $1, $4, $5, $6, $7}')"
+echo -e "Last 5 completed login sessions:\n$(sudo last | grep -vE "reboot|still logged in" | head -n 5 | awk '{print $1, $4, $5, $6, $7}')"
