cfd6449b7d6612ec3c409962a591e8680cf58fa7
root <root@meltem.meltem>
2025-10-21 08:45:40 -0500

    4. Wordlist Mode

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=Raw-SHA256 --wordlist=/usr/share/wordlists/rockyou.txt "$1"; echo "$(john --show --format=Raw-SHA256 "$1" | grep -E '^[a-zA-Z0-9].*:' | cut -d: -f1)" > 4-password.txt
+john --wordlist=/home/meltem/rockyou.txt $1 --format=Raw-MD5 > 4-password.txt
